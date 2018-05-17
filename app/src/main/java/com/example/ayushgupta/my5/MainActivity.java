0b1e4c4dd50d1d2960cc76bbaca9f9d9876cc3f3
HATERS\Ayush Gupta <ayushkgupta1998@gmail.com>
2018-05-17 17:25:06 +0530

    Now you can see images by clicking it.

--- app/src/main/java/com/example/ayushgupta/my5/MainActivity.java
+++ app/src/main/java/com/example/ayushgupta/my5/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.ayushgupta.my5;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+    final MyAdapter myAdapter = new MyAdapter(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +28,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.lv1);
         grantPermission();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                File f = (File) myAdapter.getItem(position);
+                Intent intent = new Intent(MainActivity.this,FullscreenActivity.class);
+                intent.putExtra("file",f);
+                startActivity(intent);
+                }
+        });
     }
     private void grantPermission(){
         int status = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
         if (status == PackageManager.PERMISSION_GRANTED){
             Toast.makeText(this,"Access the files",Toast.LENGTH_SHORT).show();
-            listView.setAdapter(new MyAdapter(this));
+            listView.setAdapter(myAdapter);
         }
         else{
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
--- app/src/main/java/com/example/ayushgupta/my5/MyAdapter.java
+++ app/src/main/java/com/example/ayushgupta/my5/MyAdapter.java
@@ -9,8 +9,16 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
+
+class ViewHolder{
+    TextView tv1;
+    TextView tv2;
+    ImageView iv1;
+    ImageView iv2;
+}
 
 public class MyAdapter extends BaseAdapter
 {
@@ -35,7 +43,7 @@ public class MyAdapter extends BaseAdapter
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return files[position];
     }
 
     @Override
@@ -46,26 +54,38 @@ public class MyAdapter extends BaseAdapter
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = LayoutInflater.from(mActivity);
-        View v = inflater.inflate(R.layout.my_list,parent);
-        ImageView iv1 = v.findViewById(R.id.iv1);
-        TextView tv1 = v.findViewById(R.id.tv1);
-        TextView tv2 = v.findViewById(R.id.tv2);
-        ImageView iv2 = v.findViewById(R.id.iv2);
         final File f = files[position];
+        ViewHolder viewHolder;
+        if (convertView == null){
+            convertView = inflater.inflate(R.layout.my_list,parent,false);
+            viewHolder = new ViewHolder();
+            viewHolder.iv1 = convertView.findViewById(R.id.iv1);
+            viewHolder.tv1 = convertView.findViewById(R.id.tv1);
+            viewHolder.tv2 = convertView.findViewById(R.id.tv2);
+            viewHolder.iv2 = convertView.findViewById(R.id.iv2);
+
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
         Bitmap bmp = BitmapFactory.decodeFile(f.getPath());
-        Bitmap bmp1 = ThumbnailUtils.extractThumbnail(bmp, 40, 40);
-        iv1.setImageBitmap(bmp1);
-        tv1.setText(f.getName());
-        String str = String.valueOf(f.length() / 1024.0) + " kb";
-        tv2.setText(str);
-        iv2.setOnClickListener(new View.OnClickListener() {
+        Bitmap bmp1 = ThumbnailUtils.extractThumbnail(bmp,40,40);
+        viewHolder.iv1.setImageBitmap(bmp1);
+        viewHolder.tv1.setText(f.getName());
+        String s = String.valueOf(f.length() / 1024) + " kb";
+        viewHolder.tv2.setText(s);
+        viewHolder.iv2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                f.delete();
+                boolean b = f.delete();
+                if (!b){
+                    Toast.makeText(mActivity,"Can't delete the file!!", Toast.LENGTH_SHORT).show();
+                }
                 files = file.listFiles();
                 MyAdapter.this.notifyDataSetChanged();
             }
         });
-        return v;
+
+        return convertView;
     }
 }
